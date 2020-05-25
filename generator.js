101310822e679d5c674aa1bc37ad7e88226eece5
ljnjiannan <ljnjiannan@163.com>
2020-05-25 20:41:38 +0800

    init

---
+++ generator.js
@@ -0,0 +1,11 @@
+module.exports = (api, options, rootOptions) => {
+  // 修改 `package.json` 里的字段
+  api.extendPackage({
+    dependencies: {
+      "vant": "^2.8.3",
+      "lodash": "^4.17.15",
+      "axios": "^0.19.2"
+    }
+  })
+
+}
