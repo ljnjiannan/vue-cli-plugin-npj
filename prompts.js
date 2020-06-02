cc0896198a6597615d773a276f020900a8b6b4e1
ljnjiannan <ljnjiannan@163.com>
2020-06-02 10:15:37 +0800

    fix prompts api

--- prompts.js
+++ prompts.js
@@ -1,18 +1,14 @@
-module.exports = pkg => {
-  const prompts = [
-    {
-      name: 'projectMode',
-      message: 'Choose project mode',
-      value: 'my-feature',
-      type: 'list',
-      choices: [
-        'signle project',
-        'multi project'
-      ]
-    }
-  ]
+module.exports = api => {
 
-  return prompts
-  
+  api.injectPrompt({
+    name: 'projectMode',
+    message: 'Choose project mode',
+    value: 'my-feature',
+    type: 'list',
+    choices: [
+      'signle project',
+      'multi project'
+    ]
+  })
 
 }
